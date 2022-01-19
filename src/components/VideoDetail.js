425b4987c7924e323f32fd49e615a0a1aa418ef6
abilash abi <abimugunthan2000@gmail.com>
2022-01-19 11:43:13 +0530

    channal name and date added

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,17 +1,23 @@
 import React from "react";
+import "./App.css";
 
 const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Loading...</div>;
   }
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+  const date = video.snippet.publishTime;
+  const returnDate = date.slice(0, 10);
+
   return (
     <div>
       <div className="ui embed">
         <iframe title="videoplayer" src={videoSrc} />
       </div>
       <div>
-        <h3>{video.snippet.title}</h3>
+        <h3 className="video-title">{video.snippet.title}</h3>
+        <h2>{video.snippet.channelTitle}</h2>
+        <h4>Published on : {returnDate}</h4>
         <p>{video.snippet.description}</p>
       </div>
     </div>
